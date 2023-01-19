0a378a2759ce37a62952e5ffe27d4e04099bfe95
yangjiyoung <0112jyoung@naver.com>
2023-01-19 23:23:33 +0900

    feat: item entity 설정

---
+++ src/main/java/com/example/jpastudy/constant/ItemSellStatus.java
@@ -0,0 +1,5 @@
+package com.example.jpastudy.constant;
+
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
---
+++ src/main/java/com/example/jpastudy/entity/Item.java
@@ -0,0 +1,39 @@
+package com.example.jpastudy.entity;
+
+import com.example.jpastudy.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name="item")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, length=50)
+    private String itemName;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stockNumber;
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus; // 상품 판매 상태
+
+    private LocalDateTime regTime; // 등록 시간
+    private LocalDateTime updateTime; //수정 시간
+}
