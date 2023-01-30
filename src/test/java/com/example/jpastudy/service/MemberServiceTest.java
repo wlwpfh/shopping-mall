592fef5e23400abab905573a9943fbce950a4f8d
yangjiyoung <0112jyoung@naver.com>
2023-01-30 16:18:47 +0900

    test: 중복 회원 가입 테스트 설정

--- src/test/java/com/example/jpastudy/service/MemberServiceTest.java
+++ src/test/java/com/example/jpastudy/service/MemberServiceTest.java
@@ -43,4 +43,17 @@ class MemberServiceTest {
         assertEquals(member.getPassword(), savedMember.getPassword());
         assertEquals(member.getRole(), savedMember.getRole());
     }
+
+    @Test
+    @DisplayName("중복 회원 가입 테스트")
+    void saveDuplicateMember() {
+        Member member1=createMember();
+        Member member2=createMember();
+
+        memberService.saveMember(member1);
+        Throwable e= assertThrows(IllegalStateException.class, () ->{
+           memberService.saveMember(member2);
+        });
+        assertEquals("이미 가입된 회원입니다.", e.getMessage());
+    }
 }
