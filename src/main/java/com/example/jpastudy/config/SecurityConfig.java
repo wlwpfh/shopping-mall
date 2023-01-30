07f292f8c4c4e04e7fbd91c1fa2c9711c1e9abfc
yangjiyoung <0112jyoung@naver.com>
2023-01-30 16:15:15 +0900

    feat: securityconfig 수정

--- src/main/java/com/example/jpastudy/config/SecurityConfig.java
+++ src/main/java/com/example/jpastudy/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.jpastudy.config;
 
+import com.example.jpastudy.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,7 +12,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig {
+
     protected void configure(HttpSecurity http) throws Exception{
 
     }
