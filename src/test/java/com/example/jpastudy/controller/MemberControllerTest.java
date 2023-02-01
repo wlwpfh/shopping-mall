62b337ccdc43088115b66c8b13855c41f2b89aa1
yangjiyoung <0112jyoung@naver.com>
2023-02-01 20:43:42 +0900

    test: 로그인 실패 테스트 구현

--- src/test/java/com/example/jpastudy/controller/MemberControllerTest.java
+++ src/test/java/com/example/jpastudy/controller/MemberControllerTest.java
@@ -55,4 +55,17 @@ class MemberControllerTest {
                 .user(email).password(password))
                 .andExpect(SecurityMockMvcResultMatchers.authenticated());
     }
+
+    @Test
+    @DisplayName("로그인 실패 테스트")
+    public void loginFailTest() throws Exception{
+        String email="test@gmail.com";
+        String password="1234";
+        this.createMember(email, password);
+
+        mockMvc.perform(formLogin().userParameter("email")
+                        .loginProcessingUrl("/members/login")
+                        .user(email).password("12345"))
+                .andExpect(SecurityMockMvcResultMatchers.unauthenticated());
+    }
 }
