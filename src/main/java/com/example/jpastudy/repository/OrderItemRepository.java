09beb7063010b5153a019d5cc201801e04c9c6e4
yangjiyoung <0112jyoung@naver.com>
2023-02-10 22:35:25 +0900

    feat: 주문 아이템 레포지토리 설정

---
+++ src/main/java/com/example/jpastudy/repository/OrderItemRepository.java
@@ -0,0 +1,8 @@
+package com.example.jpastudy.repository;
+
+import com.example.jpastudy.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    
+}
