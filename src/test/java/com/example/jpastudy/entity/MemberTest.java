de523c9337713a4e47c9f5870b90f00f3e11d09d
yangjiyoung <0112jyoung@naver.com>
2023-02-15 17:05:16 +0900

    test: Member에 대한 BaseEntity 테스트

---
+++ src/test/java/com/example/jpastudy/entity/MemberTest.java
@@ -0,0 +1,40 @@
+package com.example.jpastudy.entity;
+
+import com.example.jpastudy.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
+public class MemberTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @DisplayName("Auditing 테스트")
+    @WithMockUser(username="jiyoung", roles="USER")
+    void auditingTest(){
+        Member newMember=new Member();
+        memberRepository.save(newMember);
+
+        em.flush();
+        em.clear();
+
+        Member member=memberRepository.findById(newMember.getId())
+                .orElseThrow(EntityNotFoundException::new);
+        System.out.println("register time: "+ member.getRegTime());
+        System.out.println("update time: "+member.getUpdateTime());
+        System.out.println("create member: "+member.getCreatedBy());
+        System.out.println("update member: "+ member.getModifiedBy());
+    }
+}
