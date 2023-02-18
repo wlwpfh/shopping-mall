6b642159c9af5beb851a301f1654afc36ed793bd
yangjiyoung <0112jyoung@naver.com>
2023-02-18 22:57:18 +0900

    feat: 상품 데이터 정보 전달 dto 설정

---
+++ src/main/java/com/example/jpastudy/dto/ItemFormDto.java
@@ -0,0 +1,46 @@
+package com.example.jpastudy.dto;
+
+import com.example.jpastudy.constant.ItemSellStatus;
+import com.example.jpastudy.entity.Item;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class ItemFormDto {
+    private Long id;
+
+    @NotBlank(message=" 상품명은 필수 입력 값입니다.")
+    private String itemName;
+
+    @NotBlank(message = "가격은 필수 입력 값입니다.")
+    private Integer price;
+
+    @NotBlank(message = "상세는 필수 입력 값입니다.")
+    private String itemDetail;
+
+    @NotNull
+    private Integer stockNumber;
+
+    private ItemSellStatus itemSellStatus;
+
+    private List<ItemImageDto> itemImageDtoList=new ArrayList<>();
+
+    private List<Long> itemImageIds=new ArrayList<>();
+
+    private static ModelMapper modelMapper=new ModelMapper();
+
+    public Item createItem(){
+        return modelMapper.map(this, Item.class);
+    }
+
+    public static ItemFormDto of(Item item){
+        return modelMapper.map(item, ItemFormDto.class);
+    }
+}
