6925d7f3eeaba550580423eb93c923c762d2552e
yangjiyoung <0112jyoung@naver.com>
2023-02-21 23:12:05 +0900

    feat: 파일 저장을 위한 인터페이스 생성

---
+++ src/main/java/com/example/jpastudy/repository/ItemImageRepository.java
@@ -0,0 +1,8 @@
+package com.example.jpastudy.repository;
+
+import com.example.jpastudy.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImageRepository extends JpaRepository<ItemImg, Long> {
+
+}
