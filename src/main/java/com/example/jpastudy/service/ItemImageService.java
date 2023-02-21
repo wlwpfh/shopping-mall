b73e855557acbffd5f9781551ea421f45f695dca
yangjiyoung <0112jyoung@naver.com>
2023-02-21 23:19:13 +0900

    feat: 상품 이미지 정보 저장

---
+++ src/main/java/com/example/jpastudy/service/ItemImageService.java
@@ -0,0 +1,38 @@
+package com.example.jpastudy.service;
+
+import com.example.jpastudy.entity.ItemImage;
+import com.example.jpastudy.repository.ItemImageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.util.StringUtils;
+
+import javax.transaction.Transactional;
+import java.io.File;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ItemImageService {
+    @Value("${itemImageLocation}")
+    private String itemImageLocation;
+
+    private final ItemImageRepository itemImageRepository;
+
+    private final FileService fileService;
+
+    public void saveItemImage(ItemImage itemImage, MultipartFile itemImageFile) throws Exception{
+        String originalImageName=itemImageFile.getOriginalFilename();
+        String imageName="";
+        String imageUrl="";
+
+        if(!StringUtils.isEmpty(originalImageName)){
+            imageName=fileService.uploadFile(itemImageLocation, originalImageName, itemImageFile.getBytes());
+            imageUrl="/images/item/"+imageName;
+        }
+
+        itemImage.updateItemImg(originalImageName, imageName, imageUrl);
+        itemImageRepository.save(itemImage);
+    }
+}
