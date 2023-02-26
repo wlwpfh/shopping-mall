09a8d2c3d825c0cb88b48098d0c83caa0a8358db
yangjiyoung <0112jyoung@naver.com>
2023-02-26 14:39:50 +0900

    feat: id값을 통해 아이템 찾기 추가

--- src/main/java/com/example/jpastudy/repository/ItemImageRepository.java
+++ src/main/java/com/example/jpastudy/repository/ItemImageRepository.java
@@ -3,6 +3,8 @@ package com.example.jpastudy.repository;
 import com.example.jpastudy.entity.ItemImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemImageRepository extends JpaRepository<ItemImage, Long> {
+import java.util.List;
 
+public interface ItemImageRepository extends JpaRepository<ItemImage, Long> {
+    List<ItemImage> findByItemIdOrderByAsc(Long itemId);
 }
