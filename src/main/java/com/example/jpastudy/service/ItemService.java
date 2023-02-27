2978b7f1b8b0f0135a3d3f9891314d3a2bb4c542
yangjiyoung <0112jyoung@naver.com>
2023-02-27 16:25:02 +0900

    feat: 상품 데이터 읽어오기 전용 설정

--- src/main/java/com/example/jpastudy/service/ItemService.java
+++ src/main/java/com/example/jpastudy/service/ItemService.java
@@ -1,15 +1,18 @@
 package com.example.jpastudy.service;
 
 import com.example.jpastudy.dto.ItemFormDto;
+import com.example.jpastudy.dto.ItemImageDto;
 import com.example.jpastudy.entity.Item;
 import com.example.jpastudy.entity.ItemImage;
 import com.example.jpastudy.repository.ItemImageRepository;
 import com.example.jpastudy.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.transaction.Transactional;
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,19 +20,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository itemRepository;
+    private final ItemImageRepository itemImageRepository;
     private final ItemImageService itemImageService;
 
-    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImageFileList) throws Exception{
+    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImageFileList) throws Exception {
         // 1. 상품 저장
-        Item item= itemFormDto.createItem();
+        Item item = itemFormDto.createItem();
         itemRepository.save(item);
 
         // 2. 이미지 등록
-        for(int i=0;i<itemImageFileList.size();i++){
-            ItemImage itemImage=new ItemImage();
+        for (int i = 0; i < itemImageFileList.size(); i++) {
+            ItemImage itemImage = new ItemImage();
             itemImage.setItem(item);
 
-            if(i==0) //대표 이미지로 설정
+            if (i == 0) //대표 이미지로 설정
                 itemImage.setRepImgYn("Y");
             else
                 itemImage.setRepImgYn("N");
@@ -37,5 +41,25 @@ public class ItemService {
             itemImageService.saveItemImage(itemImage, itemImageFileList.get(i));
         }
         return item.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public ItemFormDto getItemDetail(Long itemId){
+        List<ItemImage> itemImageList=itemImageRepository.findByItemIdOrderByItemIdAsc(itemId);
+        List<ItemImageDto> itemImageDtoList = new ArrayList<>();
+
+        for(ItemImage itemImage: itemImageList){
+            ItemImageDto itemImageDto=ItemImageDto.of(itemImage);
+            itemImageDtoList.add(itemImageDto);
+        }
+
+        Item item= itemRepository.findById(itemId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        ItemFormDto itemFormDto=ItemFormDto.of(item);
+
+        itemFormDto.setItemImageDtoList(itemImageDtoList);
+
+        return itemFormDto;
     }
 }
