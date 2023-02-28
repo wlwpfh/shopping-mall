8a4193bcadcfee8196b21dd7e184f7cd1ce0023d
yangjiyoung <0112jyoung@naver.com>
2023-02-28 17:12:20 +0900

    feat: 아이템에 비즈니스 로직 추가

--- src/main/java/com/example/jpastudy/entity/Item.java
+++ src/main/java/com/example/jpastudy/entity/Item.java
@@ -1,6 +1,7 @@
 package com.example.jpastudy.entity;
 
 import com.example.jpastudy.constant.ItemSellStatus;
+import com.example.jpastudy.dto.ItemFormDto;
 import com.fasterxml.jackson.databind.ser.Serializers;
 import lombok.Getter;
 import lombok.Setter;
@@ -44,4 +45,12 @@ public class Item extends BaseEntity {
             inverseJoinColumns = @JoinColumn(name="item_id")
     )
     private List<Member> members;
+
+    public void updateItem(ItemFormDto itemFormDto){
+        this.itemName=itemFormDto.getItemName();
+        this.price=itemFormDto.getPrice();
+        this.stockNumber=itemFormDto.getStockNumber();
+        this.itemDetail=itemFormDto.getItemDetail();
+        this.itemSellStatus=itemFormDto.getItemSellStatus();
+    }
 }
