aea9464ef3f57500ccdf565b4efebd0db2cad6d3
yangjiyoung <0112jyoung@naver.com>
2023-03-14 14:33:18 +0900

    feat: 상품 생성 s3 연결

--- src/main/java/com/example/jpastudy/controller/ItemController.java
+++ src/main/java/com/example/jpastudy/controller/ItemController.java
@@ -12,10 +12,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.EntityNotFoundException;
@@ -36,17 +33,17 @@ public class ItemController {
 
     @PostMapping(value = "/admin/item/new")
     public String itemNew(@Valid ItemFormDto itemFormDto, BindingResult bindingResult, Model model,
-                          @RequestParam("itemImageFile") List<MultipartFile> itemImageFileList) {
+                          @RequestParam("itemImageFile") MultipartFile file) {
         if (bindingResult.hasErrors())
             return "item/itemForm";
 
-        if (itemImageFileList.get(0).isEmpty() && itemFormDto.getId() == null) {
-            model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력값입니다.");
+        if (file.isEmpty()) {
+            model.addAttribute("errorMessage", "상품 이미지는 필수 입력값입니다.");
             return "item/itemForm";
         }
 
         try {
-            itemService.saveItem(itemFormDto, itemImageFileList);
+            itemService.saveItem(itemFormDto, file);
         } catch (Exception e) {
             model.addAttribute("errorMessage", "상품 등록 중 에러가 발생하였습니다.");
             return "item/itemForm";
--- src/main/java/com/example/jpastudy/service/ItemService.java
+++ src/main/java/com/example/jpastudy/service/ItemService.java
@@ -1,16 +1,11 @@
 package com.example.jpastudy.service;
 
 import com.example.jpastudy.dto.ItemFormDto;
-import com.example.jpastudy.dto.ItemImageDto;
 import com.example.jpastudy.dto.ItemSearchDto;
 import com.example.jpastudy.dto.MainItemDto;
 import com.example.jpastudy.entity.Item;
-import com.example.jpastudy.entity.ItemImage;
-import com.example.jpastudy.repository.ItemImageRepository;
 import com.example.jpastudy.repository.ItemRepository;
-import groovy.util.logging.Log;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -26,45 +21,26 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository itemRepository;
-    private final ItemImageRepository itemImageRepository;
     private final ItemImageService itemImageService;
+    private final S3FileService fileService;
 
-    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImageFileList) throws Exception {
+    public Long saveItem(ItemFormDto itemFormDto, MultipartFile file) throws Exception {
         // 1. 상품 저장
         Item item = itemFormDto.createItem();
-        itemRepository.save(item);
-
+        String imageUrl=fileService.uploadFile(file, file.getOriginalFilename());
         // 2. 이미지 등록
-        for (int i = 0; i < itemImageFileList.size(); i++) {
-            ItemImage itemImage = new ItemImage();
-            itemImage.setItem(item);
-
-            if (i == 0) //대표 이미지로 설정
-                itemImage.setRepImgYn("Y");
-            else
-                itemImage.setRepImgYn("N");
-
-            itemImageService.saveItemImage(itemImage, itemImageFileList.get(i));
-        }
+        item.setImageUrl(imageUrl);
+        itemRepository.save(item);
         return item.getId();
     }
 
     @Transactional(readOnly = true)
     public ItemFormDto getItemDetail(Long itemId){
-        List<ItemImage> itemImageList=itemImageRepository.findByItemIdOrderByItemIdAsc(itemId);
-        List<ItemImageDto> itemImageDtoList = new ArrayList<>();
-
-        for(ItemImage itemImage: itemImageList){
-            ItemImageDto itemImageDto=ItemImageDto.of(itemImage);
-            itemImageDtoList.add(itemImageDto);
-        }
 
         Item item= itemRepository.findById(itemId)
                 .orElseThrow(EntityNotFoundException::new);
 
         ItemFormDto itemFormDto=ItemFormDto.of(item);
-
-        itemFormDto.setItemImageDtoList(itemImageDtoList);
 
         return itemFormDto;
     }
@@ -74,11 +50,6 @@ public class ItemService {
                 .orElseThrow(EntityNotFoundException::new);
         item.updateItem(itemFormDto);
 
-        List<Long> itemImageIds=itemFormDto.getItemImageIds();
-
-        for(int i=0;i<itemImageList.size();i++){
-            itemImageService.updateItemImage(itemImageIds.get(i), itemImageList.get(i));
-        }
         return item.getId();
     }
 
