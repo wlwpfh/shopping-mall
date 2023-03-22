ea59c469f4fef2f33640d07e0cef5a2c11f7664a
yangjiyoung <0112jyoung@naver.com>
2023-03-22 20:37:47 +0900

    feat: 주문 내역 연결

--- src/main/java/com/example/jpastudy/controller/OrderController.java
+++ src/main/java/com/example/jpastudy/controller/OrderController.java
@@ -2,21 +2,26 @@ package com.example.jpastudy.controller;
 
 
 import com.example.jpastudy.dto.OrderDto;
+import com.example.jpastudy.dto.OrderListDto;
 import com.example.jpastudy.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.lang.reflect.Field;
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
 
 @Controller
 @RequiredArgsConstructor
@@ -42,5 +47,18 @@ public class OrderController {
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+    }
+
+    @GetMapping(value = {"/orders", "/orders/{page}"})
+    public String orderHist(@PathVariable("page") Optional<Integer> page, Principal principal, Model model){
+        Pageable pageable= PageRequest.of(page.isPresent()? page.get() : 0,4);
+
+        Page<OrderListDto> orderListDtos=orderService.getOrderList(principal.getName(), pageable);
+
+        model.addAttribute("orders", orderListDtos);
+        model.addAttribute("page", pageable.getPageNumber());
+        model.addAttribute("maxPage", 5);
+
+        return "order/orderHist";
     }
 }
