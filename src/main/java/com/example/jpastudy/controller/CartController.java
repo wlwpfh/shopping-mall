19c070a534214b7a0c4ce86f1406a778f9ef183e
yangjiyoung <0112jyoung@naver.com>
2023-03-26 15:49:55 +0900

    feat: 장바구니 추가 매핑

---
+++ src/main/java/com/example/jpastudy/controller/CartController.java
@@ -0,0 +1,44 @@
+package com.example.jpastudy.controller;
+
+import com.example.jpastudy.dto.CartItemDto;
+import com.example.jpastudy.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class CartController {
+    private final CartService cartService;
+
+    @PostMapping(value = "/cart")
+    public @ResponseBody ResponseEntity order(@RequestBody @Valid CartItemDto cartItemDto, BindingResult bindingResult, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            List<FieldError> fieldErrorList = bindingResult.getFieldErrors();
+            for (FieldError fieldError : fieldErrorList) {
+                sb.append(fieldError.getDefaultMessage());
+            }
+            return new ResponseEntity(sb.toString(), HttpStatus.BAD_REQUEST);
+        }
+        String email = principal.getName();
+        Long cartItemId;
+
+        try {
+            cartItemId = cartService.addCart(cartItemDto, email);
+        } catch (Exception e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(cartItemId, HttpStatus.OK);
+    }
+}
