9aca9d5cb2e893fcfc874f878c352d15449d14f9
yangjiyoung <0112jyoung@naver.com>
2023-03-30 15:26:18 +0900

    feat: 장바구니 조회 로직 설정

--- src/main/java/com/example/jpastudy/repository/CartItemRepository.java
+++ src/main/java/com/example/jpastudy/repository/CartItemRepository.java
@@ -1,8 +1,17 @@
 package com.example.jpastudy.repository;
 
+import com.example.jpastudy.dto.CartDetailDto;
 import com.example.jpastudy.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findByCartIdAndItemId(Long cartId, Long itemId);
+
+    @Query("select new com.example.jpastudy.dto.CartDetailDto(ci.id, i.itemName, i.price, ci.count, i.imageUrl)" +
+        "from CartItem ci join ci.item i where ci.cart.id = :cartId and i.id = ci.item.id order by ci.regTime desc"
+    )
+    List<CartDetailDto> findCartDetailDtoList(Long cartId);
 }
