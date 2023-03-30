5bbcca4043201e658686b903e4ebdfd5e5c05c4d
yangjiyoung <0112jyoung@naver.com>
2023-03-30 15:35:55 +0900

    feat: 장바구니 조회 페이지 설정

--- src/main/java/com/example/jpastudy/controller/CartController.java
+++ src/main/java/com/example/jpastudy/controller/CartController.java
@@ -1,13 +1,17 @@
 package com.example.jpastudy.controller;
 
+import com.example.jpastudy.dto.CartDetailDto;
 import com.example.jpastudy.dto.CartItemDto;
 import com.example.jpastudy.service.CartService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -40,5 +44,14 @@ public class CartController {
             return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity(cartItemId, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/cart")
+    public String orderHistory(Principal principal, Model model){
+        List<CartDetailDto> cartDetailDtoList=cartService.getCartList(principal.getName());
+
+        model.addAttribute("cartItems", cartDetailDtoList);
+
+        return "cart/cartList";
     }
 }
