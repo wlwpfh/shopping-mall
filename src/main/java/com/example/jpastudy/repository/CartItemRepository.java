42ce82b70d4f80357d6ca31fef17224d36c81389
yangjiyoung <0112jyoung@naver.com>
2023-04-03 17:30:06 +0900

    feat: 장바구니 목록 및 수정 설정

--- src/main/java/com/example/jpastudy/repository/CartItemRepository.java
+++ src/main/java/com/example/jpastudy/repository/CartItemRepository.java
@@ -10,8 +10,11 @@ import java.util.List;
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findByCartIdAndItemId(Long cartId, Long itemId);
 
-    @Query("select new com.example.jpastudy.dto.CartDetailDto(ci.id, i.itemName, i.price, ci.count, i.imageUrl)" +
-        "from CartItem ci join ci.item i where ci.cart.id = :cartId and i.id = ci.item.id order by ci.regTime desc"
+    @Query("select new com.example.jpastudy.dto.CartDetailDto(ci.id, ci.item.itemName, ci.item.price, ci.count, ci.item.imageUrl) " +
+            "from CartItem ci " +
+            "where ci.cart.id = :cartId " +
+            "order by ci.regTime desc"
     )
     List<CartDetailDto> findCartDetailDtoList(Long cartId);
+
 }
